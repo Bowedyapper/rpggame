8a30d1545b5a3d72fce31edf5598572d296465ca
bowedyapper <bowedyapper@live.co.uk>
2022-06-14 18:28:07 +0100

    added image loader
    removed hardcoded width and height

--- game.cpp
+++ game.cpp
@@ -2,16 +2,23 @@ class Game {
 public:
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-	int HEIGHT, WIDTH;
-	Game() {}
-	Game(int height, int width) {
-		HEIGHT = height;
-		WIDTH = width;
+	Game() {
 		#if EE_CURRENT_PLATFORM == EE_PLATFORM_WINDOWS
 		printf("Environment is windows, setting audio driver to Directsound\n");
 			SDL_setenv("SDL_AUDIODRIVER", "directsound", true);
 		#endif
 			bool ttfInit = TTF_Init();
+
+
+			// load support for the JPG and PNG image formats
+			int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+			int initted = IMG_Init(flags);
+			if ((initted & flags) != flags) {
+				printf("IMG_Init: Failed to init required jpg and png support!\n");
+				printf("IMG_Init: %s\n", IMG_GetError());
+				// handle error
+			}
+
 			printf("Initalising SDL..");
 		if (SDL_Init(SDL_INIT_EVERYTHING) != 0 && ttfInit)
 		{
@@ -23,7 +30,7 @@ public:
 		 window = SDL_CreateWindow(windowTitle,
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED,
-			WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
+			windowWidth, windowHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
 		if (!window)
 		{
 			printf("error");
@@ -49,8 +56,8 @@ public:
 	}
 
 	void resizeWindow(int w, int h) {
-		WIDTH = w;
-		HEIGHT = h;
+		windowWidth = w;
+		windowHeight = h;
 		SDL_SetWindowSize(window, w, h);
 
 	}
