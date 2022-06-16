9aff906d679f6b5fc1b062e1c824a8d8c94d5ec7
bowedyapper <bowedyapper@live.co.uk>
2022-06-16 22:17:25 +0100

    comments to give a quick overview

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 ﻿#include "headers.h"
 int main(int argc, char* argv[]) {
-	Game game(800, 600, "MF");
+
+	Game game(800, 600, "MF"); // Create game (creates window and renderer)
+
+	//Socket connection-y stuff
 	Socket socket("http://rpg.json.scot");
 	socket.emit("user_wants_connection");
 	socket.on("user_got_connected", [&](sio::event &ev) {
-		printf("Connected mf\n");
+		printf("Connected to server\n");
 		});
 
 
-	game.mainTextures.loadTexture("map", "assets/textures/map.png");
-	
-	TextureObject mapT = game.mainTextures.getTexture("map");
-	SDL_Rect textureRect = { 0, 0, mapT.width, mapT.height };
+	game.mainTextures.loadTexture("map", "assets/textures/map.png"); //load and store in cache
+	TextureObject mapT = game.mainTextures.getTexture("map"); // get from cache
+	SDL_Rect textureRect = { 0, 0, mapT.width, mapT.height }; // rect for applying texture
+
 	while (game.isRunning) {
-		game.pollEvents();
-		game.calculateDeltaTime();
-		game.clearScreen();
+		game.pollEvents(); // poll events (mouse, keys etc)
+		game.calculateDeltaTime(); // calculate delta since last frame
+		game.clearScreen(); // clear whole screen
 
-		SDL_RenderCopy(game.renderer, mapT.texture, NULL, &textureRect);
+		SDL_RenderCopy(game.renderer, mapT.texture, NULL, &textureRect); // apply texture to rect
 
-		game.renderPresent();
+		game.renderPresent(); // present everything to the renderer
 
 		//Enable this to see mouse position
 		//std::cout << "X: " << game.mousePosX << " Y: " << game.mousePosY << std::endl;
-		
 	}
 	return 0;
 }
