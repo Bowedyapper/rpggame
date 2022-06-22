0000d08a4c0134ded0538b0040c0d4f12fcac3b8
bowedyapper <bowedyapper@live.co.uk>
2022-06-23 00:05:03 +0100

    new pointers for game and char more debug text latency detection removed vulkan rendering (back to openGL)

--- main.cpp
+++ main.cpp
@@ -10,20 +10,22 @@ std::vector<sio::message::ptr> playerChunk;
 std::string currentUserSocketId;
 sio::client client;
 
-Game game(800, 600, "AoTJ"); // Create game (creates window and renderer)
-auto c = game.mainTextures.loadTexture("player", "assets/textures/scot.png");
-TextureObject charTexture = game.mainTextures.getTexture("player");
-Character player("player", charTexture.texture, 1539, 2076);
+int lastFpsUpdate = 0;
+int fps = 0;
+
+Game *game = new Game(1280, 720, "AoTJ"); // Create game (creates window and renderer);
+Character* player = new Character("player", NULL, 1539, 2076);
+
 extern const int LEVEL_WIDTH = 5832;
 extern const int LEVEL_HEIGHT = 4320;
 void movementHandler(Socket* socket) {
 	const Uint8* keystates = SDL_GetKeyboardState(NULL);
 	if (/*keystates[SDL_SCANCODE_LEFT] || */ keystates[SDL_SCANCODE_A]) {
 		
-		if (player.move("left")) {
+		if (player->move("left")) {
 			sio::message::list move("l");
-			move.push(sio::double_message::create(game.delta));
-			move.push(sio::int_message::create(player.rect.x));
+			move.push(sio::double_message::create(game->delta));
+			move.push(sio::int_message::create(player->rect.x));
 			//move.push(sio::int_message::create(ms));
 			socket->emit("move", move);
 		}
@@ -31,10 +33,10 @@ void movementHandler(Socket* socket) {
 
 	if (/*keystates[SDL_SCANCODE_RIGHT] ||*/ keystates[SDL_SCANCODE_D]) {
 		
-		if (player.move("right")) {
+		if (player->move("right")) {
 			sio::message::list move("r");
-			move.push(sio::double_message::create(game.delta));
-			move.push(sio::int_message::create(player.rect.x));
+			move.push(sio::double_message::create(game->delta));
+			move.push(sio::int_message::create(player->rect.x));
 			//move.push(sio::int_message::create(ms));
 			socket->emit("move", move);
 		}
@@ -43,10 +45,10 @@ void movementHandler(Socket* socket) {
 
 	if (/*keystates[SDL_SCANCODE_UP] ||*/ keystates[SDL_SCANCODE_W]) {
 		
-		if (player.move("up")) {
+		if (player->move("up")) {
 			sio::message::list move("u");
-			move.push(sio::double_message::create(game.delta));
-			move.push(sio::int_message::create(player.rect.y));
+			move.push(sio::double_message::create(game->delta));
+			move.push(sio::int_message::create(player->rect.y));
 			//move.push(sio::int_message::create(ms));
 			socket->emit("move", move);
 		}
@@ -54,10 +56,10 @@ void movementHandler(Socket* socket) {
 
 	if (/*keystates[SDL_SCANCODE_DOWN] ||*/ keystates[SDL_SCANCODE_S]) {
 		
-		if (player.move("down")) {
+		if (player->move("down")) {
 			sio::message::list move("d");
-			move.push(sio::double_message::create(game.delta));
-			move.push(sio::int_message::create(player.rect.y));
+			move.push(sio::double_message::create(game->delta));
+			move.push(sio::int_message::create(player->rect.y));
 			//move.push(sio::int_message::create(ms));
 			socket->emit("move", move);
 		}
@@ -66,20 +68,20 @@ void movementHandler(Socket* socket) {
 
 	// Camera movement (Arrow keys)
 	if (keystates[SDL_SCANCODE_UP]) {
-		player.camera.y -= (1 * 0.2) * game.delta;
+		player->camera.y += (-1 * 0.2) * game->delta;
 
 	}
 
 	if (keystates[SDL_SCANCODE_DOWN]) {
-		player.camera.y += (1 * 0.2) * game.delta;
+		player->camera.y += (1 * 0.2) * game->delta;
 	}
 
 	if (keystates[SDL_SCANCODE_LEFT]) {
-		player.camera.x -= (1 * 0.2) * game.delta;
+		player->camera.x += (-1 * 0.2) * game->delta;
 	}
 
 	if (keystates[SDL_SCANCODE_RIGHT]) {
-		player.camera.x += (1 * 0.2) * game.delta;
+		player->camera.x += (1 * 0.2) * game->delta;
 	}
 }
 
@@ -98,7 +100,7 @@ void gameChunkUpdate(sio::event& evnt) {
 				findUser->pos(x, y);
 			}
 			else {
-				Character newPlayer(user, charTexture.texture);
+				Character newPlayer(user, player->charTexture);
 				playerVector.push_back(newPlayer);
 			}
 		}
@@ -110,7 +112,7 @@ void drawRemotePlayers() {
 		if (playerVector[ii].socketid != currentUserSocketId) {
 			std::cout << playerVector[ii].rect.x << " " << playerVector[ii].rect.x << std::endl;
 			playerVector[ii].pos(playerVector[ii].x, playerVector[ii].y);
-			playerVector[ii].drawRemote(player.camera);
+			playerVector[ii].drawRemote(player->camera);
 		}
 	}
 }
@@ -132,93 +134,100 @@ void handleUserDisconnect(sio::event& evnt) {
 	}
 }
 
-int cmdInputToDebugConsole() {
-	std::string hello;
-	while (std::cin >> hello) {
-		if (game.isRunning == false)
-			break;
-		std::cout << hello << std::endl;
-	}
-	return 0;
+bool loadTextures() {
+	game->mainTextures.loadTexture("player", "./assets/textures/scot.png");
+	game->mainTextures.loadTexture("map", "./assets/textures/map.png");
+
+	return true;
 }
 
 int main(int argc, char* argv[]) {
+
+	// TODO: Need to figure out why this will not work correctly in the init funciton within the game class
 	SDL_Init(SDL_INIT_AUDIO);
 	initAudio();
-	//playMusic("assets/audio/audio.wav", 5);
-	
-	//Socket connection-y stuff
-	Socket socket("http://rpg.json.scot");
-	socket.emit("user_wants_connection");
-	socket.on("user_got_connected", [&](sio::event &ev) {
-		printf("Connected to server\n");
-		});
 
-	socket.on("user_got_connected", &onConnection);
-	socket.on("game_chunk_update", &gameChunkUpdate);
-	socket.on("user_disconnect", &handleUserDisconnect);
-	game.mainTextures.loadTexture("map", "assets/textures/map.png"); //load and store in cache
+	// Load the game textures, will be seperated per scene so we only load what is needed
+	bool load = loadTextures();
+	if (!load) {
+		std::cout << "Error loading textures" << std::endl;
+	}
 	
-	TextureObject mapT = game.mainTextures.getTexture("map"); // get from cache
-	
-	SDL_Rect textureRect = { 0,0, mapT.width, mapT.height }; // rect for applying texture
+	// Get textures from VRAM and give pointers
+	TextureObject charTexture = game->mainTextures.getTexture("player");
+	TextureObject mapT = game->mainTextures.getTexture("map");
 
-	while (game.isRunning) {
+	SDL_Rect textureRect = { 0,0, mapT.width, mapT.height }; // Rect used for world map (will hoepfully be tilemaps later
+
+	player->applyTexture(charTexture.texture); // Copy texture to player rect
+
+	playMusic("assets/audio/audio.wav", 5);
+	
+	
+	// Socket connection-y stuff
+	Socket socket("http://178.128.38.39:4545"); // Connects to server
+	socket.emit("user_wants_connection"); // Tell server we want to connect
+
+	socket.on("user_got_connected", &onConnection); // Handle connection success
+	//socket.on("game_chunk_update", &gameChunkUpdate); // Handle game chunk updates
+	socket.on("user_disconnect", &handleUserDisconnect); // Handle remote user DC's
+	
+	// Main game loop
+	while (game->isRunning) {
+
+		// Update FPS counter every 1s
+		if (clock() - lastFpsUpdate > 1000) {
+			lastFpsUpdate = clock();
+			fps = game->calcFps();
+		}
+
+		socket.checkLatency(5000);
 		movementHandler(&socket);
-		game.pollEvents(); // poll events (mouse, keys etc)
-		game.calculateDeltaTime(); // calculate delta since last frame
-		game.clearScreen(); // clear whole screen
-		
-		//SDL_RenderCopy(game.renderer, mapT.texture, NULL, &textureRect); // apply texture to rect
-		
-		
-		//Enable this to see mouse position
-		//std::cout << "X: " << game.mousePosX << " Y: " << game.mousePosY << std::endl;
-		
+		game->pollEvents(); // poll events (mouse, keys etc)
+		game->calculateDeltaTime(); // calculate delta since last frame
+		game->clearScreen(); // clear whole screen
+	
 
-		//player.camera.x = (player.x + player.size / 2) - (game.windowWidth / 2);
-		//player.camera.y = (player.y + player.size / 2) - (game.windowHeight / 2);
-		if (player.camera.x < 0)
+		player->camera.x = (player->x + player->size / 2) - (game->windowWidth / 2);
+		player->camera.y = (player->y + player->size / 2) - (game->windowHeight / 2);
+		if (player->camera.x < 0)
 		{
-			player.camera.x = 0;
+			player->camera.x = 0;
 		}
-		if (player.camera.y < 0)
+		if (player->camera.y < 0)
 		{
-			player.camera.y = 0;
+			player->camera.y = 0;
 		}
-		if (player.camera.x > LEVEL_WIDTH - player.camera.w)
+		if (player->camera.x > LEVEL_WIDTH - player->camera.w)
 		{
-			player.camera.x = LEVEL_WIDTH - player.camera.w;
+			player->camera.x = LEVEL_WIDTH - player->camera.w;
 		}
-		if (player.camera.y > LEVEL_HEIGHT - player.camera.h)
+		if (player->camera.y > LEVEL_HEIGHT - player->camera.h)
 		{
-			player.camera.y = LEVEL_HEIGHT - player.camera.h;
+			player->camera.y = LEVEL_HEIGHT - player->camera.h;
 		}
 
 		
-		game.clipRenderTexture(mapT.texture, NULL, NULL, 0, 0, &player.camera);
-		player.draw();
-		drawRemotePlayers();
+		game->clipRenderTexture(mapT.texture, NULL, NULL, 0, 0, &player->camera);
+		
+		//drawRemotePlayers();
+		player->draw();
 
-		std::string mousePos = "Mouse Position - X: " + std::to_string(game.mousePosX) + " Y: " + std::to_string(game.mousePosY);
-		std::string charPos = "Character Position - X: " + std::to_string(player.x) + " Y: " + std::to_string(player.y);
-		std::string camPos = "Camera Position - X: " + std::to_string(player.camera.x) + " Y: " + std::to_string(player.camera.y);
-		std::string fpsText = "FPS: " + std::to_string((int)(game.calcFps()));
+		std::string mousePos = "Mouse Position - X: " + std::to_string(game->mousePosX) + " Y: " + std::to_string(game->mousePosY);
+		std::string charPos = "Character Position - X: " + std::to_string(player->x) + " Y: " + std::to_string(player->y);
+		std::string camPos = "Camera Position - X: " + std::to_string(player->camera.x) + " Y: " + std::to_string(player->camera.y);
+		std::string fpsText = "FPS: " + std::to_string(fps);
+		std::string chatOpenText = "ChatOpen: " + std::to_string(game->chatOpen);
+		std::string latencyText = "Latency: " + std::to_string(socket.latency) + "ms";
 
-		char mousePosBuffer[100];
-		char charPosBuffer[100];
-		char camPosBuffer[100];
-		char fpsBuffer[100];
-		strcpy_s(mousePosBuffer, 100, mousePos.c_str());
-		strcpy_s(charPosBuffer, 100, charPos.c_str());
-		strcpy_s(camPosBuffer, 100, camPos.c_str());
-		strcpy_s(fpsBuffer, 100, fpsText.c_str());
-		game.displayText("assets/font/font.ttf", 10, mousePosBuffer, 1, 1, black);
-		game.displayText("assets/font/font.ttf", 10, charPosBuffer, 1, 15, black);
-		game.displayText("assets/font/font.ttf", 10, camPosBuffer, 1, 30, black);
-		game.displayText("assets/font/font.ttf", 10, fpsBuffer, 1, 45, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, mousePos, 1, 1, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, charPos, 1, 15, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, camPos, 1, 30, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, fpsText, 1, 45, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, chatOpenText, 1, 60, black);
+		game->displayText("assets/font/font.ttf", game->windowHeight * 0.02, latencyText, 1, 75, black);
 
-		game.render(); // present everything to the renderer
+		game->render(); // present everything to the renderer
 
 	}
 	return 0;
