dd241ab1a3d2c9ccf427df72e34060617c19d4f5
bowedyapper <bowedyapper@live.co.uk>
2022-06-23 17:42:53 +0100

    no longer use cpu consumed time using chrono steady clock

--- classes/socket.h
+++ classes/socket.h
@@ -2,29 +2,30 @@
 #include <sio_client.h>
 #include <sio_message.h>
 
-extern Game *game;
+extern Game* game;
 class Socket {
 
 public:
-	int lastLatencyCheck = clock();
+	std::chrono::steady_clock::time_point lastLatencyCheck = std::chrono::steady_clock::now();
 	std::chrono::steady_clock::time_point lastLatencyPacketSent = std::chrono::steady_clock::now();
 	std::chrono::steady_clock::time_point lastLatencyPacketRecieved = std::chrono::steady_clock::now();
 	int latency = 0;
 	Socket(char* socketHost) {
+		lastLatencyCheck = std::chrono::steady_clock::now();
 		client.connect(socketHost);
 		on("latency_response", [&](sio::event& ev) {
 			lastLatencyPacketRecieved = std::chrono::steady_clock::now();
 			latency = std::chrono::duration_cast<std::chrono::milliseconds>(lastLatencyPacketRecieved - lastLatencyPacketSent).count();
-		});
-		
+			});
+
 	}
 	void on(std::string eventName, const sio::socket::event_listener func);
 	void emit(std::string const& name, sio::message::list const& msglist, std::function<void(sio::message::list const&)> const& ack);
 	void checkLatency(int interval);
-private: 
+private:
 	sio::client client;
 	bool connect(char* socketHost);
-	
+
 };
 
 bool Socket::connect(char* socketHost) {
@@ -41,10 +42,11 @@ void Socket::emit(std::string const& name, sio::message::list const& msglist = N
 	client.socket()->emit(name, msglist, ack);
 }
 
-void Socket::checkLatency(int interval){
-	if (clock() - lastLatencyCheck > interval) {
+void Socket::checkLatency(int interval) {
+	double elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - lastLatencyCheck).count();
+	if (elapsed_time > interval) {
 		emit("latency_check");
 		lastLatencyPacketSent = std::chrono::steady_clock::now();
-		lastLatencyCheck = clock();
+		lastLatencyCheck = std::chrono::steady_clock::now();
 	}
 }
